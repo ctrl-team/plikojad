650db728c98dd5d610e4f9061a7d3280d631bbfa
Nimplex <frekerhugug@gmail.com>
2020-03-06 18:41:00 +0100

    Bug fixes and /file endpoint

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ let express = require("express");
 let app = express();
 
 let db = require("./db/");
-let bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -15,17 +13,29 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.get("/api/add", async (req, res) => {
-  let body = req["body"];
-  if (!body["code"] || !body["title"]) {
+app.get("/api/add/:title", async (req, res) => {
+  let title = req.params["title"]; // /api/add/<title>
+  let code = req.query["code"]; // /api/add/<title>?code=<code>
+  if (code.length < 2 || !title) {
     return res.status("400").send("Bad Request");
   }
   let length = await r
     .table(rethinkdb.table)
     .count()
     .run(global.conn);
-  db.addFile({ code: body["code"], title: body["title"], id: length + 1 });
-  res.status("200").send("OK");
+  db.addFile({ code: code, title: title, id: length + 1 });
+  res.redirect(`/file/${length + 1}`);
+});
+
+app.get("/file/:id", async (req, res) => {
+  let id = req.params.id;
+  let file = await r
+    .table(rethinkdb.table)
+    .filter({ id: parseInt(id) })
+    .coerceTo("array")
+    .run(global.conn);
+  let code = file[0]["code"];
+  let title = file[0]["title"];
 });
 
 app.get("*", (req, res) => {
