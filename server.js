d12ef610df81c3d5ca9f8f3ebdc86b858c1ed43c
Nimplex <frekerhugug@gmail.com>
2020-03-06 14:11:35 +0100

    File adding system to db

--- server.js
+++ server.js
@@ -5,12 +5,27 @@ let express = require("express");
 let app = express();
 
 let db = require("./db/");
+let bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Welcome");
+});
+
+app.get("/api/add", async (req, res) => {
+  let body = req["body"];
+  if (!body["code"] || !body["title"]) {
+    return res.status("400").send("Bad Request");
+  }
+  let length = await r
+    .table(rethinkdb.table)
+    .count()
+    .run(global.conn);
+  db.addFile({ code: body["code"], title: body["title"], id: length + 1 });
+  res.status("200").send("OK");
 });
 
 app.get("*", (req, res) => {
